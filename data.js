104ead4a300557cfffe5b95ff50621b84eef6d49
gvozdev.viktor <gvozdev.viktor@gmail.com>
2012-09-29 11:54:54 +0300

    test data

---
+++ data.js
@@ -0,0 +1,24 @@
+var data = {
+    prop1: 123,
+    prop2: 'qwe',
+    prop3: {
+        prop1: 456,
+        prop2: 'asd',
+        prop3: {
+            prop1: 789,
+            prop2: 'zxc',
+            prop3: 123
+        }
+    },
+    arr: [
+        {
+            prop: '123'
+        },
+        {
+            prop: '456'
+        },
+        {
+            prop: '789'
+        }
+    ]
+};
