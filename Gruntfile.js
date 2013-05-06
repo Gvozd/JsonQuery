e5e20ef8565924d35ad2936bb67503b5eb5b6a0c
gvozdev.viktor <gvozdev.viktor@gmail.com>
2013-05-06 13:55:46 +0300

    Clean up after build

--- Gruntfile.js
+++ Gruntfile.js
@@ -85,7 +85,10 @@ module.exports = function (grunt) {
             jison: {
                 cmd: 'cd lib && jison grammar.jison'
             }
-        }
+        },
+        clean: [
+            'lib/grammar.js'
+        ]
     });
 
     // These plugins provide necessary tasks.
@@ -93,10 +96,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-exec');
 
     // Default task.
-    grunt.registerTask('default', ['jshint', 'exec', 'concat', 'uglify', 'nodeunit']);
+    grunt.registerTask('default', ['jshint', 'exec', 'concat', 'clean', 'nodeunit', 'uglify']);
 
 };
