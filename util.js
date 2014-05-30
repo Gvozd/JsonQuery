b5e31beade7550b0ed9011196fb5948ac54ec1c4
gvozd <gvozdev.viktor@gmail.com>
2014-05-31 01:42:20 +0400

    optimize util & add builtin then

--- util.js
+++ util.js
@@ -4,127 +4,155 @@ var util = (function () {
     var util = {};
 
     util.text = function text(needle) {
-        return function exprText(haystack, position) {
-            if (haystack.substr(position, needle.length) === needle) {
-                return {
-                    res: needle,
-                    end: position + needle.length
-                };
-            }
-        };
+        var result = exprText.bind(util, needle);
+        result.then = util.then.bind(util, result);
+        return result;
     };
 
+    function exprText(needle, haystack, position) {
+        if (haystack.substr(position, needle.length) === needle) {
+            return {
+                res: needle,
+                end: position + needle.length
+            };
+        }
+    }
+
     util.regexp = function (needle) {
+        var result;
         if ('string' === typeof needle) {
             needle = new RegExp('^(?:' + needle + ')');
         }
-        return function exprRegexp(haystack, position) {
-            var m;
-
-            needle.lastIndex = position;
-            m = needle.exec(haystack);
-
-            if (m && m.index === 0) {
-                return {
-                    res: m[0],
-                    end: position + m[0].length
-                };
-            }
-        };
+        result = exprRegexp.bind(util, needle);
+        result.then = util.then.bind(util, result);
+        return result;
     };
+
+    function exprRegexp(needle, haystack, position) {
+        var m;
+        needle.lastIndex = position;
+        m = needle.exec(haystack);
+        if (m && m.index === 0) {
+            return {
+                res: m[0],
+                end: position + m[0].length
+            };
+        }
+    }
 
     util.optional = function optional(pattern) {
-        return function combinatorOptional(haystack, position) {
-            return pattern(haystack, position) || {
-                res: undefined,
-                end: position
-            };
-        };
+        var result = combinatorOptional.bind(util, pattern);
+        result.then = util.then.bind(util, result);
+        return result;
     };
+
+    function combinatorOptional(pattern, haystack, position) {
+        return pattern(haystack, position) || {
+            res: undefined,
+            end: position
+        };
+    }
 
     util.exclude = function exclude(pattern, except) {
-        return function combinatorExclude(haystack, position) {
-            return !except(haystack, position) &&
-                pattern(haystack, position);
-        };
+        var result = combinatorExclude.bind(util, pattern, except);
+        result.then = util.then.bind(util, result);
+        return result;
     };
+
+    function combinatorExclude(pattern, except, haystack, position) {
+        return !except(haystack, position) && pattern(haystack, position);
+    }
 
     util.any = function any(firstPattern) {
-        var patterns = 'function' === typeof firstPattern ? arguments : firstPattern;
-        return function combinatorAny(haystack, position) {
-            var result,
-                i;
-
-            for (i = 0; i < patterns.length; i += 1) {
-                result = patterns[i](haystack, position);
-                if (result) {
-                    return result;
-                }
-            }
-        };
+        var patterns = 'function' === typeof firstPattern ? arguments : firstPattern,
+            result = combinatorAny.bind(util, patterns);
+        result.then = util.then.bind(util, result);
+        return result;
     };
+
+    function combinatorAny(patterns, haystack, position) {
+        var result,
+            i;
+        for (i = 0; i < patterns.length; i += 1) {
+            result = patterns[i](haystack, position);
+            if (result) {
+                return result;
+            }
+        }
+    }
 
     util.sequence = function sequence(firstPattern) {
-        var patterns = 'function' === typeof firstPattern ? arguments : firstPattern;
-        return function combinatorSequence(haystack, position) {
-            var result = [],
-                end = position,
-                r,
-                i;
-
-            for (i = 0; i < patterns.length; i += 1) {
-                r = patterns[i](haystack, end);
-                if (!r) {
-                    return;
-                }
-                result.push(r.res);
-                end = r.end;
-            }
-
-            return {
-                res: result,
-                end: end
-            };
-        };
+        var patterns = 'function' === typeof firstPattern ? arguments : firstPattern,
+            result = combinatorSequence.bind(util, patterns);
+        result.then = util.then.bind(util, result);
+        return result;
     };
 
+    function combinatorSequence(patterns, haystack, position) {
+        var result = [],
+            end = position,
+            r,
+            i;
+
+        for (i = 0; i < patterns.length; i += 1) {
+            r = patterns[i](haystack, end);
+            if (!r) {
+                return;
+            }
+            result.push(r.res);
+            end = r.end;
+        }
+
+        return {
+            res: result,
+            end: end
+        };
+    }
+
+
+    util.repeat = function repeat(pattern, separator) {
+        var separated = !separator ?
+                pattern :
+                util.then(util.sequence(separator, pattern), getFirst),
+            result = combinatorRepeat.bind(util, pattern, separated);
+        result.then = util.then.bind(util, result);
+        return result;
+    };
+
+    function combinatorRepeat(pattern, separated, haystack, position) {
+        var result = [],
+            end = position,
+            r = pattern(haystack, end);
+
+        while (r && r.end > end) {
+            result.push(r.res);
+            end = r.end;
+            r = separated(haystack, end);
+        }
+
+        return {
+            res: result,
+            end: end
+        };
+    }
 
     function getFirst(r) {
         return r[1];
     }
 
-    util.repeat = function repeat(pattern, separator) {
-        var separated = !separator ?
-                pattern :
-                util.then(util.sequence(separator, pattern), getFirst);
-
-        return function combinatorRepeat(haystack, position) {
-            var result = [],
-                end = position,
-                r = pattern(haystack, end);
-
-            while (r && r.end > end) {
-                result.push(r.res);
-                end = r.end;
-                r = separated(haystack, end);
-            }
-
-            return {
-                res: result,
-                end: end
-            };
-        };
-    };
-
     util.then = function then(pattern, transform) {
-        return function combinatorThen(haystack, position) {
-            var r = pattern(haystack, position);
-            return r && {
-                res: transform(r.res),
-                end: r.end
-            };
-        };
+        var result = combinatorThen.bind(util, pattern, transform);
+        result.then = util.then.bind(util, result);
+        return result;
     };
+
+    function combinatorThen(pattern, transform, haystack, position) {
+        var r = pattern(haystack, position);
+        return r && {
+            res: transform(r.res),
+            end: r.end
+        };
+    }
 
     return util;
 }());
