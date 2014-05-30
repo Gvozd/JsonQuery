3cd6f199b5cc28d07be2d719b63cf452b26a4085
gvozd <gvozdev.viktor@gmail.com>
2014-05-31 00:04:22 +0400

    check & fix tokens 1

--- grammar.js
+++ grammar.js
@@ -2,7 +2,7 @@
 (function () {
     'use strict';
     //lexical tokens 1
-    var n1 = '\\n|\\r\\n|\\r|\\f',                                      // nl        \n|\r\n|\r|\f
+    var nl = '\\n|\\r\\n|\\r|\\f',                                      // nl        \n|\r\n|\r|\f
         nonascii = '[^\\0-\\177]',                                      // nonascii  [^\0-\177]
         num = '[0-9]+|[0-9]*\\.[0-9]+',                                 // num       [0-9]+|[0-9]*\.[0-9]+
         unicode = '\\\\[0-9a-f]{1,6}(?:\\r\\n|[ \\n\\r\\t\\f])?',       // unicode   \\[0-9a-f]{1,6}(\r\n|[ \n\r\t\f])?
@@ -11,23 +11,23 @@
         nmchar = '[_a-z0-9-]|(?:' + nonascii + ')|(?:' + escape + ')',  // nmchar    [_a-z0-9-]|{nonascii}|{escape}
         name = '(?:' + nmchar + ')+',                                   // name      {nmchar}+
         ident = '[-]?(?:' + nmstart + ')(?:' + nmchar + ')*',           // ident     [-]?{nmstart}{nmchar}*
-        string1 = '\\"([^\\n\\r\\f\\\\"]|\\\\(?:' + n1 + ')|(?:' +
+        string1 = '\\"([^\\n\\r\\f\\\\"]|\\\\(?:' + nl + ')|(?:' +
             nonascii + ')|(?:' + escape + '))*\\"',                     // string1   \"([^\n\r\f\\"]|\\{nl}|{nonascii}|{escape})*\"
-        string2 = "\\'([^\\n\\r\\f\\\\']|\\\\(?:" + n1 + ")|(?:" +
-            nonascii + ")|(?:" + escape + "))*\\'",                     // string2   \'([^\n\r\f\\']|\\{nl}|{nonascii}|{escape})*\'
+        string2 = '\\\'([^\\n\\r\\f\\\\\']|\\\\(?:' + nl + ')|(?:' +
+            nonascii + ')|(?:' + escape + '))*\\\'',                     // string2   \'([^\n\r\f\\']|\\{nl}|{nonascii}|{escape})*\'
         string = '(?:' + string1 + ')|(?:' + string2 + ')',             // string    {string1}|{string2}
-        invalid1 = '\\"([^\\n\\r\\f\\\\"]|\\\\{nl}|(?:' +
+        invalid1 = '\\"([^\\n\\r\\f\\\\"]|\\\\(?:' + nl + ')|(?:' +
             nonascii + ')|(?:' + escape + '))*',                        // invalid1  \"([^\n\r\f\\"]|\\{nl}|{nonascii}|{escape})*
-        invalid2 = "\\'([^\\n\\r\\f\\\\']|\\\\{nl}|(?:" +
-            nonascii + ")|(?:" + escape + "))*",                        // invalid2  \'([^\n\r\f\\']|\\{nl}|{nonascii}|{escape})*
-        invalid = '(?:' + invalid1 + ')|(?:' + invalid2 + ')',                          // invalid   {invalid1}|{invalid2}
+        invalid2 = '\\\'([^\\n\\r\\f\\\\\']|\\\\(?:' + nl + ')|(?:' +
+            nonascii + ')|(?:' + escape + '))*',                        // invalid2  \'([^\n\r\f\\']|\\{nl}|{nonascii}|{escape})*
+        invalid = '(?:' + invalid1 + ')|(?:' + invalid2 + ')',          // invalid   {invalid1}|{invalid2}
         w = '[ \\t\\r\\n\\f]*',                                         // w         [ \t\r\n\f]*
         D = 'd|\\\\0{0,4}(44|64)(\\r\\n|[ \\t\\r\\n\\f])?',             // D         d|\\0{0,4}(44|64)(\r\n|[ \t\r\n\f])?
         E = 'e|\\\\0{0,4}(45|65)(\\r\\n|[ \\t\\r\\n\\f])?',             // E         e|\\0{0,4}(45|65)(\r\n|[ \t\r\n\f])?
-        N = 'n|\\\\0{0,4}(4e|6e)(\\r\\n|[ \\t\\r\\n\\f])?|\\n',         // N         n|\\0{0,4}(4e|6e)(\r\n|[ \t\r\n\f])?|\\n
-        O = 'o|\\\\0{0,4}(4f|6f)(\\r\\n|[ \\t\\r\\n\\f])?|\\o',         // O         o|\\0{0,4}(4f|6f)(\r\n|[ \t\r\n\f])?|\\o
-        T = 't|\\\\0{0,4}(54|74)(\\r\\n|[ \\t\\r\\n\\f])?|\\t',         // T         t|\\0{0,4}(54|74)(\r\n|[ \t\r\n\f])?|\\t
-        V = 'v|\\\\0{0,4}(58|78)(\\r\\n|[ \\t\\r\\n\\f])?|\\v';         // V         v|\\0{0,4}(58|78)(\r\n|[ \t\r\n\f])?|\\v
+        N = 'n|\\\\0{0,4}(4e|6e)(\\r\\n|[ \\t\\r\\n\\f])?|\\\\n',       // N         n|\\0{0,4}(4e|6e)(\r\n|[ \t\r\n\f])?|\\n
+        O = 'o|\\\\0{0,4}(4f|6f)(\\r\\n|[ \\t\\r\\n\\f])?|\\\\o',       // O         o|\\0{0,4}(4f|6f)(\r\n|[ \t\r\n\f])?|\\o
+        T = 't|\\\\0{0,4}(54|74)(\\r\\n|[ \\t\\r\\n\\f])?|\\\\t',       // T         t|\\0{0,4}(54|74)(\r\n|[ \t\r\n\f])?|\\t
+        V = 'v|\\\\0{0,4}(58|78)(\\r\\n|[ \\t\\r\\n\\f])?|\\\\v';       // V         v|\\0{0,4}(58|78)(\r\n|[ \t\r\n\f])?|\\v
 }());
 //
 //[ \t\r\n\f]+     return S;
